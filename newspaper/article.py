46e8ea904419c743fc1f5441f0dad110979ddbad
rafi.martirosyan@picsart.com <rafi.martirosyan@picsart.com>
2023-08-01 15:18:55 +0400

    comment not needed lines

--- newspaper/article.py
+++ newspaper/article.py
@@ -24,7 +24,7 @@ from .extractors import ContentExtractor
 from .outputformatters import OutputFormatter
 from .utils import (URLHelper, RawHelper, extend_config,
                     get_available_languages, extract_meta_refresh)
-from .videos.extractors import VideoExtractor
+# from .videos.extractors import VideoExtractor
 
 log = logging.getLogger(__name__)
 
@@ -209,19 +209,22 @@ class Article(object):
         self.set_html(html)
         self.set_title(title)
 
-    def parse(self):
-        self.throw_if_not_downloaded_verbose()
+    def get_doc(self, html):
+        return self.config.get_parser().fromstring(html)
 
-        self.doc = self.config.get_parser().fromstring(self.html)
+    def parse(self, doc):
+        # self.throw_if_not_downloaded_verbose()
+
+        self.doc = doc
         self.clean_doc = copy.deepcopy(self.doc)
 
-        if self.doc is None:
+        # if self.doc is None:
             # `parse` call failed, return nothing
-            return
+        #    return
 
         # TODO: Fix this, sync in our fix_url() method
-        parse_candidate = self.get_parse_candidate()
-        self.link_hash = parse_candidate.link_hash  # MD5
+        # parse_candidate = self.get_parse_candidate()
+        # self.link_hash = parse_candidate.link_hash  # MD5
 
         document_cleaner = DocumentCleaner(self.config)
         output_formatter = OutputFormatter(self.config)
@@ -229,8 +232,8 @@ class Article(object):
         title = self.extractor.get_title(self.clean_doc)
         self.set_title(title)
 
-        authors = self.extractor.get_authors(self.clean_doc)
-        self.set_authors(authors)
+        # authors = self.extractor.get_authors(self.clean_doc)
+        # self.set_authors(authors)
 
         meta_lang = self.extractor.get_meta_lang(self.clean_doc)
         self.set_meta_language(meta_lang)
@@ -239,54 +242,54 @@ class Article(object):
             self.extractor.update_language(self.meta_lang)
             output_formatter.update_language(self.meta_lang)
 
-        meta_favicon = self.extractor.get_favicon(self.clean_doc)
-        self.set_meta_favicon(meta_favicon)
+        # meta_favicon = self.extractor.get_favicon(self.clean_doc)
+        # self.set_meta_favicon(meta_favicon)
 
-        meta_site_name = self.extractor.get_meta_site_name(self.clean_doc)
-        self.set_meta_site_name(meta_site_name)
+        # meta_site_name = self.extractor.get_meta_site_name(self.clean_doc)
+        # self.set_meta_site_name(meta_site_name)
 
-        meta_description = \
-            self.extractor.get_meta_description(self.clean_doc)
-        self.set_meta_description(meta_description)
+        # meta_description = \
+        #     self.extractor.get_meta_description(self.clean_doc)
+        # self.set_meta_description(meta_description)
 
-        canonical_link = self.extractor.get_canonical_link(
-            self.url, self.clean_doc)
-        self.set_canonical_link(canonical_link)
+        # canonical_link = self.extractor.get_canonical_link(
+        #   self.url, self.clean_doc)
+        # self.set_canonical_link(canonical_link)
 
-        tags = self.extractor.extract_tags(self.clean_doc)
-        self.set_tags(tags)
+        # tags = self.extractor.extract_tags(self.clean_doc)
+        # self.set_tags(tags)
 
-        meta_keywords = self.extractor.get_meta_keywords(
-            self.clean_doc)
-        self.set_meta_keywords(meta_keywords)
+        # meta_keywords = self.extractor.get_meta_keywords(
+        #     self.clean_doc)
+        # self.set_meta_keywords(meta_keywords)
 
-        meta_data = self.extractor.get_meta_data(self.clean_doc)
-        self.set_meta_data(meta_data)
+        # meta_data = self.extractor.get_meta_data(self.clean_doc)
+        # self.set_meta_data(meta_data)
 
-        self.publish_date = self.extractor.get_publishing_date(
-            self.url,
-            self.clean_doc)
+        # self.publish_date = self.extractor.get_publishing_date(
+        #    self.url,
+        #    self.clean_doc)
 
         # Before any computations on the body, clean DOM object
         self.doc = document_cleaner.clean(self.doc)
 
         self.top_node = self.extractor.calculate_best_node(self.doc)
         if self.top_node is not None:
-            video_extractor = VideoExtractor(self.config, self.top_node)
-            self.set_movies(video_extractor.get_videos())
+            # video_extractor = VideoExtractor(self.config, self.top_node)
+            # self.set_movies(video_extractor.get_videos())
 
             self.top_node = self.extractor.post_cleanup(self.top_node)
-            self.clean_top_node = copy.deepcopy(self.top_node)
+            # self.clean_top_node = copy.deepcopy(self.top_node)
 
             text, article_html = output_formatter.get_formatted(
                 self.top_node)
             self.set_article_html(article_html)
             self.set_text(text)
 
-        self.fetch_images()
+        # self.fetch_images()
 
         self.is_parsed = True
-        self.release_resources()
+        # self.release_resources()
 
     def fetch_images(self):
         if self.clean_doc is not None:
